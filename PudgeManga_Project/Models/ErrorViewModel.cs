2d5b75d625fe76c0e6cb0b6f8f6869cd6d9bcfaa
Vladyslav <vladmelhnik@gmail.com>
2023-10-06 20:05:31 +0300

    Add project files.

---
+++ PudgeManga_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PudgeManga_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
