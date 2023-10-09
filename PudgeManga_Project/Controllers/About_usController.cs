345ae2e57af0b275a26cd054a31e97f7a9f6c5bc
Vladyslav <vladmelhnik@gmail.com>
2023-10-09 14:56:03 +0300

    Adapted header, footer, create basic controllers and views for nav panel

---
+++ PudgeManga_Project/Controllers/About_usController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PudgeManga_Project.Controllers
+{
+    public class About_usController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PudgeManga_Project/Controllers/ForumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PudgeManga_Project.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
