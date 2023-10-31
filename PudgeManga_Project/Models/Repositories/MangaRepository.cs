891232f93fcb1a67d82a49504c9a4385252209f9
Vladyslav <vladmelhnik@gmail.com>
2023-10-31 21:14:18 +0200

    Return all changes from broken branches

---
+++ PudgeManga_Project/Models/Repositories/MangaRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PudgeManga_Project.Data;
+using PudgeManga_Project.ViewModels;
+
+namespace PudgeManga_Project.Models.Repositories
+{
+    public class MangaRepository : IRepository<Manga, int>
+    {
+        private readonly ApplicationDBContext context;
+        public MangaRepository(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task Delete(Manga manga)
+        {
+            context.Remove(manga);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Manga>> GetAll()
+        {
+            return await context.Mangas.ToListAsync();
+        }
+
+        public async Task<Manga> GetById(int id)
+        {
+            return await context.Mangas
+                .Include(ch => ch.Chapters)
+                .Include(comm => comm.Comments)
+                .Include(popul => popul.Popularity)
+                .FirstOrDefaultAsync(i => i.MangaId == id);
+        }
+
+        public async Task<Manga> Add(Manga entity)
+        {
+            await context.Mangas.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+        public async Task UpdateAsync(Manga entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            context.Mangas.Update(entity);
+            await context.SaveChangesAsync();
+        }
+        public async Task Save()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ PudgeManga_Project/ViewModels/IndexMangaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PudgeManga_Project.ViewModels
+{
+    public class IndexMangaViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string CoverUrl { get; set; }
+        public int GenreId { get; set; }
+    }
+}
