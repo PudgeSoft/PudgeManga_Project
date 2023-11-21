73eb93163f356c884eadc229a3378ffe430e20db
Vadim <vadim.zakletskyi@gmail.com>
2023-11-22 00:23:11 +0200

    Add View and Controller for search

---
+++ PudgeManga_Project/Controllers/SearchController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PudgeManga_Project.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
