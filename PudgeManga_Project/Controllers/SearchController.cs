73b81e1d1747d339c8849ccec4d5d4adbfd5222f
Vadim <vadim.zakletskyi@gmail.com>
2023-11-25 17:14:23 +0200

    Add search functionality

--- PudgeManga_Project/Controllers/SearchController.cs
+++ PudgeManga_Project/Controllers/SearchController.cs
@@ -1,13 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using PudgeManga_Project.Data;
 
 namespace PudgeManga_Project.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDBContext _context;
+        public SearchController(ApplicationDBContext context)
         {
+            _context = context;
+        }
 
-            return View();
+
+        public async Task<IActionResult> Index(string SearchString)
+        {
+            ViewData["CurrentFilter"] = SearchString;
+            var manga = from m in _context.Mangas
+                        select m;
+            if(!String.IsNullOrEmpty(SearchString) )
+            {
+                manga = manga.Where(m => m.Title.Contains(SearchString));
+            }
+
+            return View(manga);
         }
     }
 }
