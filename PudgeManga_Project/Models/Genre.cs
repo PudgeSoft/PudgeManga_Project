9cd4507f12a0fe7d9716dcf335b041824140134a
Vadim <vadim.zakletskyi@gmail.com>
2023-11-26 00:02:08 +0200

    Add MangaGenre, update View

---
+++ PudgeManga_Project/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PudgeManga_Project.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreId { get; set; }
+        public string Title {get; set; }
+
+        public ICollection<MangaGenre> MangaGenres { get; set; }
+
+    }
+}
