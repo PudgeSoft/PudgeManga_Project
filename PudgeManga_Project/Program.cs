c1a3326cb47f452edc1ff74d1c37e8653ba4a748
Vladyslav <vladmelhnik@gmail.com>
2023-11-27 19:21:32 +0200

    upd for test

--- PudgeManga_Project/Program.cs
+++ PudgeManga_Project/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddScoped<IGoogleDriveAPIRepository<IFormFile>, GoogleDriveAPIR
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection"));
 });
 
 var app = builder.Build();
