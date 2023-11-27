8c31f53df99adafd6250a65a46d273e9a30f7352
Vladyslav <vladmelhnik@gmail.com>
2023-11-27 21:39:39 +0200

    Update manga model

--- PudgeManga_Project/Models/Manga.cs
+++ PudgeManga_Project/Models/Manga.cs
@@ -5,18 +5,21 @@ namespace PudgeManga_Project.Models
     public class Manga
     {
         [Key]
-        
         public int MangaId { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
         public string CoverUrl { get; set; }
+        public string Type { get; set; }
+        public string Publish { get; set; }
+        public string Artist { get; set; }
+        public string Translator { get; set; }
 
+
+        public ICollection<MangaGenre> MangaGenres { get; set; }
         public ICollection<Chapter> Chapters { get; set; }
         public ICollection<Comment> Comments { get; set; }
         public Popularity Popularity { get; set; }
-
-        public ICollection<MangaGenre> MangaGenres { get; set; }
     }
 
 }
