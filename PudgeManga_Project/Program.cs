8fd6ad507131dd8cd98e13da05a703afd1395cb8
Vadim <vadim.zakletskyi@gmail.com>
2023-12-01 21:07:22 +0200

    Fix connection string.

--- PudgeManga_Project/Program.cs
+++ PudgeManga_Project/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 builder.Services.AddSingleton<Seed>();
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection"));
 });
 
 
