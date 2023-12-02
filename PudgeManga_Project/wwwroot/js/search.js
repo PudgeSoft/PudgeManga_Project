0bbb9fabf22edf4c84df10b8b6c1bc601344a49f
Vadim <vadim.zakletskyi@gmail.com>
2023-12-02 19:17:12 +0200

    Fix bugs with naming.

--- PudgeManga_Project/wwwroot/js/search.js
+++ PudgeManga_Project/wwwroot/js/search.js
@@ -1,12 +1,18 @@
 function animeButtonSwitch() {
     var element = document.getElementById("animeButton");
-    element.classList.toggle("btn-warning");
-    element.classList.toggle("btn-outline-warning");
+    element.classList.add("btn-warning");
+    element.classList.remove("btn-outline-warning");
+    var element = document.getElementById("mangaButton");
+    element.classList.remove("btn-warning");
+    element.classList.add("btn-outline-warning");
 }
 function mangaButtonSwitch() {
     var element = document.getElementById("mangaButton");
-    element.classList.toggle("btn-warning");
-    element.classList.toggle("btn-outline-warning");
+    element.classList.add("btn-warning");
+    element.classList.remove("btn-outline-warning");
+    var element = document.getElementById("animeButton");
+    element.classList.remove("btn-warning");
+    element.classList.add("btn-outline-warning");
 }
 function clearInput() {
     var getValue = document.getElementById("inputName");
